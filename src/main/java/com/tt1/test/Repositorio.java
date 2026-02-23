9bb7c18970a8786d1529c212815002b1947e0b0d
marioalonsocoloma <maalonco@unirioja.es>
2026-02-23 16:07:32 +0100

    Update Repositorio.java

--- src/main/java/com/tt1/test/Repositorio.java
+++ src/main/java/com/tt1/test/Repositorio.java
@@ -1,1 +1,8 @@
+package com.tt1.test;
 
+public class Repositorio {
+    public ToDo encontrarToDo(String nombre) { throw new UnsupportedOperationException("Clase aún no implementada."); }
+    public void marcarCompletado(String nombre) { throw new UnsupportedOperationException("Clase aún no implementada."); }
+    public void guardarToDo(ToDo t) { throw new UnsupportedOperationException("Clase aún no implementada."); }
+    public void guardarEmail(String email) { throw new UnsupportedOperationException("Clase aún no implementada."); }
+}
