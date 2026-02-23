fe9b577c2c3f4e144f3c7bbba5f73ca93d93c013
marioalonsocoloma <maalonco@unirioja.es>
2026-02-23 16:08:33 +0100

    Update Servicio.java

--- src/main/java/com/tt1/test/Servicio.java
+++ src/main/java/com/tt1/test/Servicio.java
@@ -1,1 +1,9 @@
+package com.tt1.test;
+import java.time.LocalDate;
 
+public class Servicio {
+    public void crearNuevaTarea(String nombre, LocalDate fecha) { throw new UnsupportedOperationException("Clase aún no implementada."); }
+    public void registrarEmail(String email) { throw new UnsupportedOperationException("Clase aún no implementada."); }
+    public void finalizarTarea(String nombre) { throw new UnsupportedOperationException("Clase aún no implementada."); }
+    public void listarTareasPendientes() { throw new UnsupportedOperationException("Clase aún no implementada."); }
+}
