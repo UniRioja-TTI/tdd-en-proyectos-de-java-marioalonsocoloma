f4d0c1c3014b52c8d8b9574a4468cfeb23bfe5c3
marioalonsocoloma <maalonco@unirioja.es>
2026-02-23 16:04:08 +0100

    Update ToDo.java

--- src/main/java/com/tt1/test/ToDo.java
+++ src/main/java/com/tt1/test/ToDo.java
@@ -1,1 +1,20 @@
+package es.unirioja.tt1;
+import java.time.LocalDate;
 
+public class ToDo {
+    private String nombre;
+    private String descripcion;
+    private LocalDate fechaLimite; // LocalDate es la forma más sencilla 
+    private boolean completado;
+
+    public ToDo() {} // Constructor vacío obligatorio de JavaBean 
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+    public LocalDate getFechaLimite() { return fechaLimite; }
+    public void setFechaLimite(LocalDate fechaLimite) { this.fechaLimite = fechaLimite; }
+    public boolean isCompletado() { return completado; }
+    public void setCompletado(boolean completado) { this.completado = completado; }
+}
