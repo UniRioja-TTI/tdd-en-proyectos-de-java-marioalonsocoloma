daaa8d3b2162d28f416f3972a4726c6ad29465d7
marioalonsocoloma <maalonco@unirioja.es>
2026-02-23 16:04:43 +0100

    Update DBSStub.java

--- src/main/java/com/tt1/test/DBSStub.java
+++ src/main/java/com/tt1/test/DBSStub.java
@@ -1,1 +1,15 @@
+package es.unirioja.tt1;
+import java.util.ArrayList;
+import java.util.List;
 
+public class DBStub {
+    private List<ToDo> tareas = new ArrayList<>(); // Colección para CRUD 
+    private List<String> agendaEmails = new ArrayList<>(); // Agenda de emails [cite: 35]
+
+    public void crear(ToDo t) { throw new UnsupportedOperationException("Clase aún no implementada."); }
+    public ToDo leer(String nombre) { throw new UnsupportedOperationException("Clase aún no implementada."); }
+    public void actualizar(ToDo t) { throw new UnsupportedOperationException("Clase aún no implementada."); }
+    public void borrar(String nombre) { throw new UnsupportedOperationException("Clase aún no implementada."); }
+    public void añadirEmail(String email) { throw new UnsupportedOperationException("Clase aún no implementada."); }
+    public List<String> obtenerEmails() { throw new UnsupportedOperationException("Clase aún no implementada."); }
+}
